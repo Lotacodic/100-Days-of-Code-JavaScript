d22fb55f4339f9acc56caf403ab00ae56df5a189
Lotacodic <Ngenejoshua05@gmail.com>
2022-12-26 20:43:20 +0100

    Clean up code

---
+++ Day 1-10/Day 03/background-color-changer/script.js
@@ -0,0 +1,16 @@
+document.querySelector(".clr1").addEventListener("click", () => {
+    document.querySelector("body").style.transition = "ease-in-out 1s";
+    document.querySelector("body").style.backgroundColor = "#9B0FDB";
+  });
+  
+  document.querySelector(".clr2").addEventListener("click", () => {
+    document.querySelector("body").style.backgroundColor = "#ffea00";
+  });
+  
+  document.querySelector(".clr3").addEventListener("click", () => {
+    document.querySelector("body").style.backgroundColor = "#04DB53";
+  });
+  
+  document.querySelector(".clr4").addEventListener("click", () => {
+    document.querySelector("body").style.backgroundColor = "#0F68DB";
+  })
